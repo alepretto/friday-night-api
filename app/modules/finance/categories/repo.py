a929dbbcf74cfd778ca5b01c0f98debf74a9fb51
Alexandre Pretto <ale.pretto@gmail.com>
2026-02-26 18:30:55 -0300

    Ajustando categories

--- app/modules/finance/categories/repo.py
+++ app/modules/finance/categories/repo.py
@@ -1,5 +1,10 @@
+import uuid
+
+from fastapi_pagination import Params
+from fastapi_pagination.ext.sqlalchemy import apaginate
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
+from sqlmodel import select
 
 from app.modules.finance.categories.exceptions import CategoryAlreadyExistis
 from app.modules.finance.categories.model import Category
@@ -21,3 +26,17 @@ class CategoryRepo:
             await self.db.rollback()
 
             raise CategoryAlreadyExistis(label=model.label)
+
+    async def get_by_id(self, id_category: uuid.UUID, user_id: uuid.UUID):
+
+        query = select(Category).where(
+            (Category.id == id_category) & (Category.user_id == user_id)
+        )
+
+        return await self.db.scalar(query)
+
+    async def list_by_user(self, user_id: uuid.UUID, params: Params | None = None):
+
+        query = select(Category).where(Category.user_id == user_id)
+
+        return await apaginate(self.db, query, params=params)
--- app/modules/finance/categories/service.py
+++ app/modules/finance/categories/service.py
@@ -1,3 +1,8 @@
+import uuid
+
+from fastapi_pagination import Params
+
+from app.modules.finance.categories.exceptions import CategoryNotFound
 from app.modules.finance.categories.model import Category
 from app.modules.finance.categories.repo import CategoryRepo
 from app.modules.finance.categories.schemas import CategoryCreate
@@ -12,3 +17,16 @@ class CategoryService:
 
         model = Category.model_validate(schema, update={"user_id": user.id})
         return await self.repo.create_update(model)
+
+    async def get_by_id(self, id_category: uuid.UUID, user: User):
+
+        category = await self.repo.get_by_id(id_category, user.id)
+
+        if not category:
+            raise CategoryNotFound()
+
+        return category
+
+    async def list_by_user(self, user: User, params: Params | None = None):
+
+        return await self.repo.list_by_user(user.id, params=params)
