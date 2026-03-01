a423108f7733c345f94aafc9d1f6b4e487481d3d
Alexandre Pretto <ale.pretto@gmail.com>
2026-02-28 22:33:22 -0300

    add feature get financial institutions

--- app/modules/finance/financial_institutions/exceptions.py
+++ app/modules/finance/financial_institutions/exceptions.py
@@ -5,3 +5,8 @@ class FinancialInstitutionsAlreadyExists(FridayNightException):
     def __init__(self, institution: str, type: str, status_code: int = 400) -> None:
         message = f"Resource already exists: {institution} - {type}"
         super().__init__(message, status_code)
+
+
+class FinancialInstitutionNotFound(FridayNightException):
+    def __init__(self, status_code: int = 404) -> None:
+        super().__init__("Financial institution not found", status_code)
--- app/modules/finance/financial_institutions/router.py
+++ app/modules/finance/financial_institutions/router.py
@@ -1,3 +1,5 @@
+import uuid
+
 from fastapi import APIRouter, Depends
 from fastapi_pagination import Page
 
@@ -21,6 +23,14 @@ async def create_financial_institution(
 ):
 
     return await service.create_update(infos_data)
+
+
+@router.get("/{institution_id}", response_model=FinancialInstitutionResponse)
+async def get_financial_institution(
+    institution_id: uuid.UUID,
+    service: FinancialInstitutionService = Depends(get_financial_institution_service),
+):
+    return await service.get_by_id(institution_id)
 
 
 @router.get("", response_model=Page[FinancialInstitutionResponse])
